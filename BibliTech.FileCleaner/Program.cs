2ac54cb7b0f78264dba877cddb5147e740b6bbe8
Luke Vo <datvm@outlook.com>
2020-04-17 22:29:45 +0700

    Add project files.

---
+++ BibliTech.FileCleaner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BibliTech.FileCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
